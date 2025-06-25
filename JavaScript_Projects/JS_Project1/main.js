563036f4f4af4424096966a386ed19621daae9eb
Bhargavp610 <bpatel06442@gmail.com>
2025-06-24 20:10:13 -0400

    Finsihed the basic UI project with JS

--- JavaScript_Projects/JS_Project1/main.js
+++ JavaScript_Projects/JS_Project1/main.js
@@ -17,9 +17,76 @@ console.log(document.querySelectorAll('.item'));
 console.log(document.getElementsByClassName('.item'));
 // We can also use by tage
 console.log(document.getElementsByTagName('li'));
-*/
 
 // to loop through it 
 const items = document.querySelectorAll('.item');
 items.forEach((item) => console.log(item));
+
+const ul = document.querySelector('.items');
+//To remove 
+ul.remove();
+// To remove last elemnt child
+ul.lastElementChild.remove();
+// To remove first elemnt child
+ul.firstElementChild.remove();
+//To change with something
+ul.firstElementChild.textContent = 'Hello';
+ul.children[1].innerText = 'Brad';
+ul.lastElementChild.innerHTML = '<h1>Hello</h1>'
+
+const btn = document.querySelector('.btn');
+btn.style.background = 'red';
+
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    //console.log('click');
+    //console.log(e.target);
+    document.querySelector('#my-form').style.background = '#ccc';
+    document.querySelector('body').classList.add('bg-dark');
+    document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
+});
+
+const btn = document.querySelector('.btn');
+// We can aslo do 'mouseover'
+btn.addEventListener('mouseout', (e) => {
+    e.preventDefault();
+    //console.log('click');
+    //console.log(e.target);
+    document.querySelector('#my-form').style.background = '#ccc';
+    document.querySelector('body').classList.add('bg-dark');
+    document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
+});
+*/
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+function onSubmit(e) {
+    e.preventDefault();
+    //console.log(nameInput.value);
+    //Once we refresh its gone there is no save database
+    
+
+    if (nameInput.value === '' || emailInput.value === ''){
+        //alert('Please enter fields');
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+        //console.log('sucess');
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+
+        userList.appendChild(li);
+
+        //clear fields
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
 
